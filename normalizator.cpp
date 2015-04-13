56fdffd32f96ba2eb3014548bc68cd23764a4164
Filip Kowalski <filipko133@gmail.com>
2015-04-13 20:12:45 +0200

    New (alternative version developed. It assures the eyes are in the right positions but may cause more distortion to the images

--- normalizator.cpp
+++ normalizator.cpp
@@ -6,14 +6,38 @@ cv::Mat Normalizator::normalize(FaceData data)
 {
 	cv::Mat face = data.image;
 
-	double angle = atan2((double) (data.leye.y - data.reye.y), (double) (data.leye.x - data.reye.x)) - CV_PI;
-	angle *= (double) (180 / CV_PI); //converting radians into degrees
+	double angle = atan2((double) (data.leye.y - data.reye.y), (double) (data.leye.x - data.reye.x)) - CV_PI; //rotation angle in RADIANS
 	//rotation
-	cv::Mat trans = cv::getRotationMatrix2D(data.faceCenter, angle, 1.0);
+	cv::Mat trans = cv::getRotationMatrix2D(data.faceCenter, (double) (180 / CV_PI) * angle, 1.0); //conversion to degrees needed
 	cv::warpAffine(face, face, trans, cv::Size(face.cols, face.rows));
-	
+	//cv::imwrite("rotated.jpg", face);
+	//now we need to calculate the new eyes' positions
+	//the goal is much easier to achieve if we use coordinates related to faceCenter
+	//std::cout << "original eyes pos: (" << data.leye.x << ", " << data.leye.y << "); (" << data.reye.x << ", " << data.reye.y << ")\n";
+	data.leye.x -= data.faceCenter.x; data.leye.y -= data.faceCenter.y;
+	data.reye.x -= data.faceCenter.x; data.reye.y -= data.faceCenter.y;
+	data.leye.y = -data.leye.y; data.reye.y = -data.reye.y; //converting to xy-Cartesian plane coordinates
+	float sina = sin(angle), cosa = cos(angle);
+	//we use the formula for new coordinates after spinning by a: (x, y) -> (xcosa - ysina, xsina + ycosa)
+	data.leye = cv::Point2f(data.leye.x * cosa - data.leye.y * sina, data.leye.x * sina + data.leye.y * cosa);
+	data.reye = cv::Point2f(data.reye.x * cosa - data.reye.y * sina, data.reye.x * sina + data.reye.y * cosa);
+
+	//back to absolute coordinates
+	data.leye.y = -data.leye.y; data.reye.y = -data.reye.y;
+	data.leye.x += data.faceCenter.x; data.leye.y += data.faceCenter.y;
+	data.reye.x += data.faceCenter.x; data.reye.y += data.faceCenter.y;
+	//std::cout << "after rotation by " << (180 / CV_PI) * angle << ": (" << data.leye.x << ", " << data.leye.y << "); (" << data.reye.x << ", " << data.reye.y << ")\n";
+	//since the positions eyes should be in are (25, 25) and (75, 25) in a 100x100 image, 
+	//we know the differnce between their x coordinate is half of the output image width desired
+	int width = 2 * (data.reye.x - data.leye.x);
+	cv::Rect output;
+	//mins and maxes are guardians so we wouldnt go out of range
+	output.x = std::max(0, (int) data.leye.x - width / 4); //if we have too little scratch of a face, so be it
+	output.y = std::max(0, (int) data.leye.y - face.rows / 4); //we cannot get the rest of it out of thin air!
+	output.width = std::min(face.cols - output.x, width);
+	output.height = std::min(face.rows - output.y, (int) data.leye.y + (3 * face.rows) / 4);
 	//cropping image based on the old face coordinates
-	face = face(cv::Rect(data.faceCenter.x - data.faceWidth / 2, data.faceCenter.y - data.faceHeight / 2, data.faceWidth, data.faceHeight)).clone();
+	face = face(output).clone();
 	cv::resize(face, face, cv::Size(NORMALIZED_WIDTH, NORMALIZED_HEIGHT));
 	return face;
 }
